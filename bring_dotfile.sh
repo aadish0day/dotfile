63e1bfa12474789fbb6fce396d0b012d1b66c847
Aadishx07 <74809669+Aadishx07@users.noreply.github.com>
2024-04-18 14:32:32 +0530

    add

--- bring_dotfile.sh
+++ bring_dotfile.sh
@@ -1,38 +1,36 @@
-
-echo "Backup i3 configuration....."
+echo "Backup i3 configuration..."
 rm -fr ~/Documents/GitHub/dotfile/i3/
 cp -r ~/.config/i3/ ~/Documents/GitHub/dotfile/
 
-echo "Backup i3 configuration....."
+echo "Backup MOC configuration..."
 rm -fr ~/Documents/GitHub/dotfile/moc/
 cp -r ~/.moc/ ~/Documents/GitHub/dotfile/moc/
 
-echo "Backup i3 configuration....."
+echo "Backup Neofetch configuration..."
 rm -fr ~/Documents/GitHub/dotfile/neofetch/
-cp -r ~/.config/neofetch/
+cp -r ~/.config/neofetch/ ~/Documents/GitHub/dotfile/neofetch/
 
-echo "Backup i3 configuration....."
+echo "Backup Picom configuration..."
 rm -fr ~/Documents/GitHub/dotfile/picom/
 cp -r ~/.config/picom/ ~/Documents/GitHub/dotfile/
 
-echo "Backup i3 configuration....."
+echo "Backup Ranger configuration..."
 rm -fr ~/Documents/GitHub/dotfile/ranger/
 cp -r ~/.config/ranger/ ~/Documents/GitHub/dotfile/
 
-echo "Backup i3 configuration....."
+echo "Backup Theme configuration..."
 rm -fr ~/Documents/GitHub/dotfile/theme/
 cp -r ~/.theme/ ~/Documents/GitHub/dotfile/theme/
 
-echo "Backup i3 configuration....."
+echo "Backup Tmux configuration..."
 rm -fr ~/Documents/GitHub/dotfile/tmux/
 cp -r ~/.config/tmux/ ~/Documents/GitHub/dotfile/
 
-echo "Backup i3 configuration....."
+echo "Backup XFCE4 configuration..."
 rm -fr ~/Documents/GitHub/dotfile/xfce4/
 cp -r ~/.config/xfce4/ ~/Documents/GitHub/dotfile/
 
-echo "Backup i3 configuration....."
+echo "Backup Zathura configuration..."
 rm -fr ~/Documents/GitHub/dotfile/zathura/
 cp -r ~/.config/zathura/ ~/Documents/GitHub/dotfile/
-
 
