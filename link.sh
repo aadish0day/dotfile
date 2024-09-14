23e5099a15fb7c9c1f5d5721807f1314382c9b41
Aadishx07 <aadishdas7@gmail.com>
2024-09-14 17:54:08 +0530

    update link.sh

--- link.sh
+++ link.sh
@@ -2,37 +2,36 @@
 
 # Directories to stow
 directories=(
-    "alacritty"
-    "bash"
-    "fastfetch"
-    "i3"
-    "kitty"
-    "moc"
-    "picom"
-    "ranger"
-    "rofi"
-    "theme"
-    "tmux"
-    "zathura"
-    "xfce4"
-    "zsh"
+	"alacritty"
+	"bash"
+	"fastfetch"
+	"i3"
+	"kitty"
+	"moc"
+	"picom"
+	"ranger"
+	"rofi"
+	"theme"
+	"tmux"
+	"zathura"
+	"xfce4"
+	"zsh"
 )
 
-# Base directory where your dotfiles are stored
-DOTFILES_DIR="$HOME/.dotfiles"
+# Base directory for dotfiles
+DOTFILES_DIR="$HOME/dotfile"
 
 # Navigate to dotfiles directory
 cd "$DOTFILES_DIR" || exit
 
-# Loop through each directory and stow it
+# Stow each directory
 for dir in "${directories[@]}"; do
-    if [ -d "$dir" ]; then
-        echo "Stowing $dir..."
-        stow --dotfiles --adopt "$dir"
-    else
-        echo "Directory $dir not found, skipping..."
-    fi
+	if [ -d "$dir" ]; then
+		echo "Stowing $dir..."
+		stow -S "$dir"
+	else
+		echo "Directory $dir not found, skipping..."
+	fi
 done
 
 echo "Dotfiles linking completed!"
-
