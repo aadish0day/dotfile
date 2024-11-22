15550f4fc130321ab3bb1f35c4f796256c214808
Aadishx07 <aadishdas7@gmail.com>
2024-11-22 23:24:29 +0530

    fix(scripts): exclude currently attached tmux session from selection list

--- scripts/.local/bin/tmux-fzf-switcher.sh
+++ scripts/.local/bin/tmux-fzf-switcher.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-# List tmux sessions and allow user to select one with fzf
-selected_session=$(tmux list-sessions -F "#{session_name}" | fzf --prompt="Select a tmux session: " --height=10 --border --layout=reverse)
+# Get the list of tmux sessions excluding the one that you're currently attached to
+attached_session=$(tmux display-message -p '#{session_name}')
+sessions=$(tmux list-sessions -F "#{session_name}" | grep -v "^$attached_session$")
 
-# If a session was selected, switch to it
-if [[ -n "$selected_session" ]]; then
-  tmux switch-client -t "$selected_session"
+# If there are sessions available, allow the user to select one
+if [[ -n "$sessions" ]]; then
+  selected_session=$(echo "$sessions" | fzf --prompt="Select a tmux session: " --height=10 --border --layout=reverse)
+
+  # If a session was selected, switch to it
+  if [[ -n "$selected_session" ]]; then
+    tmux switch-client -t "$selected_session"
+  fi
+else
+  echo "No available sessions to attach."
 fi
 
