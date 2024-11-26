680944bb7f3d5e48a6da51051e36d2b0a39216a9
Aadishx07 <aadishdas7@gmail.com>
2024-11-26 10:52:51 +0530

    refactor(dotfile): minor fix

--- scripts/.local/bin/tmux-fzf-switcher.sh
+++ scripts/.local/bin/tmux-fzf-switcher.sh
@@ -6,13 +6,12 @@ sessions=$(tmux list-sessions -F "#{session_name}" | grep -v "^$attached_session
 
 # If there are sessions available, allow the user to select one
 if [[ -n "$sessions" ]]; then
-  selected_session=$(echo "$sessions" | fzf --prompt="Select a tmux session: " --height=10 --border --layout=reverse)
+    selected_session=$(echo "$sessions" | fzf --prompt="Select a tmux session: " --height=10 --border --layout=reverse)
 
-  # If a session was selected, switch to it
-  if [[ -n "$selected_session" ]]; then
-    tmux switch-client -t "$selected_session"
-  fi
+    # If a session was selected, switch to it
+    if [[ -n "$selected_session" ]]; then
+        tmux switch-client -t "$selected_session"
+    fi
 else
-  echo "No available sessions to attach."
+    echo "No available sessions to attach."
 fi
-
