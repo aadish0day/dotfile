74c7a72e821959683d55583f6070c2f1132374cb
Aadishx07 <aadishdas7@gmail.com>
2024-12-08 22:31:10 +0530

    feat(polybar): add a polybar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,14 +1,10 @@
-#!/usr/bin/env bash
-
-# Terminate already running bar instances
-killall polybar
+# Example corrected launch.sh
+# Terminate already running Polybar instances
+killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch polybar
-polybar main -c $(dirname $0)/config.ini &
+# Launch Polybar
+polybar main &
 
-if [[ $(xrandr -q | grep 'HDMI1 connected') ]]; then
-	polybar external -c $(dirname $0)/config.ini &
-fi
