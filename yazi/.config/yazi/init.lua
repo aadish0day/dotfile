3180337af6583450c2754535c7f7a2d4e6dbf4b1
Aadishx07 <aadishdas7@gmail.com>
2025-04-06 20:59:43 +0530

    fix(yazi): update the yazi plugins

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -4,3 +4,8 @@ require("starship"):setup()
 require("zoxide"):setup({
     update_db = true,
 })
+
+require("full-border"):setup {
+	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+	type = ui.Border.ROUNDED,
+}
