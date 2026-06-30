b16acb1b136f2dac6846d0eda4af8c65444de53d
Aadishx07 <aadishdas7@gmail.com>
2026-07-01 02:34:23 +0530

    chore(yazi): remove kdeconnect-send plugin

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -6,8 +6,3 @@ require("full-border"):setup {
     -- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
     type = ui.Border.ROUNDED,
 }
-
--- KDE Connect - always show device selection
-require("kdeconnect-send"):setup({
-    auto_select_single = false,
-})
